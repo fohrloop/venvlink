7c9ae6055f5c16e467cece493cd8a39fc8cf4142
Niko Pasanen <niko@pasanen.me>
2020-12-19 19:49:47 +0200

    update PyPI info & add LICENSE.txt

--- setup.py
+++ setup.py
@@ -7,14 +7,24 @@ def find_version():
         return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", verstrline, re.M).group(1)
     except:
         raise RuntimeError("Could not find version string")
-        
+
+with open('docs/README.md', encoding='utf-8') as f:
+    long_description = f.read()  
 
 setuptools.setup(
     name='venvlink',
     description='Virtual environments centralized with activate proxy in your working dir.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
     version=find_version(),
     author='Niko Pasanen',
     author_email='niko@pasanen.me',
+    url = "https://github.com/np-8/venvlink",
     packages=setuptools.find_packages(),
-
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+    ],
 )
