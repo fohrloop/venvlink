2514bbf76f4700760d819a752d7fb305b2a761bc
Niko Pasanen <niko@pasanen.me>
2020-12-20 18:26:37 +0200

    default logging level to info (from debug)

--- venvlink/__init__.py
+++ venvlink/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
     datefmt="%m-%d %H:%M",
 )
